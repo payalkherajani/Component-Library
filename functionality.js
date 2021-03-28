6ab99c1444b6c876b671fd09f90416ee83a4c936
payalkherajani <payal.kherajani19@gmail.com>
2021-03-28 09:44:54 +0530

    used-ternary-operator

--- functionality.js
+++ functionality.js
@@ -1,18 +1,9 @@
 function toggle(id) {
     const sidebarData = document.getElementById(id);
-    if (sidebarData.style.display === 'none') {
-        sidebarData.style.display = 'block';
-    }
-    else {
-        sidebarData.style.display = 'none';
-    }
+    sidebarData.style.display === 'none' ? (sidebarData.style.display = 'block') : (sidebarData.style.display = 'none')
 }
+
 function showCode(id) {
     const imagetoShow = document.getElementById(id);
-    if (imagetoShow.style.display === 'none') {
-        imagetoShow.style.display = 'block';
-    }
-    else {
-        imagetoShow.style.display = 'none';
-    }
+    imagetoShow.style.display === 'none' ? (imagetoShow.style.display = 'block') : (imagetoShow.style.display = 'none')
 }
